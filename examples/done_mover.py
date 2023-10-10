1ff27d6667393e3353fdcc6ae6fa261af1a31b7e
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2023-10-10 14:12:32 +0200

    make the loading from repo optional

--- examples/done_mover.py
+++ examples/done_mover.py
@@ -4,13 +4,14 @@ import difflib
 from pathlib import Path
 import fire
 
-# import the library from the current repo instead of pypi
-import sys
-saved_path = sys.path
-sys.path.insert(0, "../src")
-sys.path.insert(0, "../src/LogseqMarkdownParser")
 import LogseqMarkdownParser
-sys.path = saved_path
+# # import the library from the current repo instead of pypi
+# import sys
+# saved_path = sys.path
+# sys.path.insert(0, "../src")
+# sys.path.insert(0, "../src/LogseqMarkdownParser")
+# import LogseqMarkdownParser
+# sys.path = saved_path
 
 def check_internet_connection():
     try:
