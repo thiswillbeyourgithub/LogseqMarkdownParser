3194b0f90f24b7afd2eb353f628a4dd40bc7ba52
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-07-05 11:47:07 +0200

    switch from pyproject.toml to setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="LogseqMarkdownParser",
+    version="2.8",
+    author="thiswillbeyourgithub",
+    description="parse logseq markdown text with easy access to properties, hierarchy, TODO etc",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thiswillbeyourgithub/LogseqMarkdownParser",
+    packages=find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords=["Logseq", "PKM", "Markdown", "parsing", "parser", "properties", "block", "text"],
+    install_requires=[
+        "fire>=0.5.0",
+    ],
+    python_requires=">=3.9",
+    entry_points={
+        "console_scripts": [
+            "LogseqMarkdownParser=LogseqMarkdownParser:cli",
+        ],
+    },
+    license_files=("LICENSE",),
+)
